576ef5c638d99dcc878739be4da99148a44993e3
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-01-26 13:07:49 -0700

    Bootstrap input, and working on js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
 $("document").ready(function (e){
 
-  function validateEmail(email){
-    var re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(email);
-  }
-
-  function validate(){
-  $("#result").text("");
-  var email = $("#email").val();
-  if (validateEmail(email)) {
-    alert("Successfully submitted, spam emails coming now.");
-  } else {
-    alert("Not an email!");
-  }
-  return false;
-}
-
-$("form").bind("submit", validate);
-
 });
