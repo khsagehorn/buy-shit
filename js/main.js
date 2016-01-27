68f5d3082ed23117cb990092789361d1e3bf36cb
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-01-27 14:56:17 -0700

    email success

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
 $("document").ready(function (e){
 
-  function validateEmail(email){
-    var re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(email);
-  }
+  $( ".form-horizontal" ).on("submit", function(e) {
+    alert("Email Accepted, Spam Coming now");
 
-  function validate(){
-  $("#result").text("");
-  var email = $("#email").val();
-  if (validateEmail(email)) {
-    alert("Successfully submitted, spam emails coming now.");
-  } else {
-    alert("Not an email!");
-  }
-  return false;
-}
-
-$("form").bind("submit", validate);
+  });
 
 
 $(".woodBtn").click(function(){
